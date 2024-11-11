0b65e6a38d7822151ea67600d5ce878feb7e92a7
LuisEnGuerrero <LuisEnGuerrero@yahoo.com>
2024-11-11 09:40:23 -0500

    Fix MongoClient in mongodb.py

--- utils/database.py
+++ utils/database.py
@@ -1,5 +1,3 @@
-# utils/database.py
-
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
--- utils/mongodb.py
+++ utils/mongodb.py
@@ -1,24 +1,18 @@
 from pymongo import MongoClient
 from datetime import datetime
-#import os
 import streamlit as st
-# from dotenv import load_dotenv
-
-# Cargar variables de entorno
-# load_dotenv()
-
-# Conexión a MongoDB
-#client = MongoClient(os.getenv("MONGO_URI"))
 
 # Obtener la URI de MongoDB desde los secretos de Streamlit Cloud
 MONGO_URI = st.secrets["MONGO"]["MONGO_URI"]
 
 # Crear una instancia del cliente de MongoDB
-client = MongoClient(MONGO_URI)
-
-
-db = client["motorcycle_detection"]  # Nombre de la base de datos
-collection = db["detections"]        # Nombre de la colección
+try:
+    client = MongoClient(MONGO_URI)
+    db = client["motorcycle_detection"]  # Nombre de la base de datos
+    collection = db["detections"]        # Nombre de la colección
+    st.write("Conexión a MongoDB establecida correctamente.")
+except Exception as e:
+    st.error(f"Error al conectar con MongoDB: {e}")
 
 def save_inference_result(result):
     """
@@ -32,7 +26,7 @@ def save_inference_result(result):
         "motorcycle_count": len([pred for pred in result["predictions"] if pred["class"] == "motorcycle"]),
     }
     collection.insert_one(document)
-    print(f"Resultado de inferencia guardado en MongoDB con ID {document['inference_id']}")
+    st.write(f"Resultado de inferencia guardado en MongoDB con ID {document['inference_id']}")
     return document
 
 def get_inference_statistics():
