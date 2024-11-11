cb53d547330fe87b6b91494dd7bf6e12cf62d4ae
LuisEnGuerrero <LuisEnGuerrero@yahoo.com>
2024-11-11 09:01:30 -0500

    Remove st.write in yoloconect.py

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ API_KEY = st.secrets["ROBOFLOW"]["ROBOFLOW_API_KEY"]
 MODEL_ID = st.secrets["ROBOFLOW"]["ROBOFLOW_MODEL_ID"]
 API_URL = st.secrets["ROBOFLOW"]["ROBOFLOW_API_URL"]
 
-# Agregar mensajes de depuración
-st.write(f"API_KEY: {API_KEY}")
-st.write(f"MODEL_ID: {MODEL_ID}")
-st.write(f"API_URL: {API_URL}")
-
 if not API_KEY or not MODEL_ID or not API_URL:
     raise EnvironmentError("API_KEY, MODEL_ID o API_URL no están configurados en los secretos de Streamlit")
 
